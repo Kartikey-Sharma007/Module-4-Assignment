6bdae73e0787fb6039b785a3d75181911468c8f5
Kartikey Sharma <kartikeys84@gmail.com>
2025-08-03 13:03:15 +0200

    Add files via upload

---
+++ Module 4 Assignment.py
@@ -0,0 +1,40 @@
+#  factorial of a number
+'''n = int(input('enter the number'))
+
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n * (factorial(n-1))
+
+result= factorial(n)
+print('the factorial of',n,'is:', result)
+'''
+
+
+
+
+
+
+
+
+# math operations
+'''import math
+
+n= float(input('enter the number'))
+
+print('square root:',math.sqrt(n))
+
+print('logarithm:',math.log(n))
+
+print('Sine:',math.sin(n))
+'''
+
+
+
+
+
+
+
+
+
